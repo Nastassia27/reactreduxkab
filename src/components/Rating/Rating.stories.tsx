be633b2839b93d35f2bfaf84601031c133fcb8dc
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-06 20:08:30 +0300

    install storybook

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import {Button} from "../../stories/Button";
 import {Rating, RatingValueType} from "./Rating";
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +10,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+
+
   </React.StrictMode>
 );
 
@@ -17,3 +19,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
