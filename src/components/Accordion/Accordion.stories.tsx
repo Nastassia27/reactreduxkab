23953a64121a6d07e688d8f77e41a0efe87a0797
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-06 22:09:08 +0300

    try new version of stories/unsuccessfully

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -9,13 +9,27 @@ export default{
     component: Accordion,
 };
 
-
 const Template: Story<AccordionPropsType> = (args)=><Accordion {...args}/>
 
+const xallbackProps={
+    onChange: callback,
+    onClick: onClickCallback
+}
 
 export const MenuCollapsedMode2 = Template.bind({});
 MenuCollapsedMode2.args={
     titleValue:'Menu',
+    value: true,
+
+    items:[],
+    onClick:y=>y,
+    onChange: x=>x
+
+}
+
+export const UserUncollapsedMode2 = Template.bind({});
+MenuCollapsedMode2.args={
+    titleValue:'Users',
     value: true,
     onChange: x=>x,
     items:[],
@@ -33,3 +47,49 @@ export const ModeChanging = ()=>{
 
 
 }
+/*import type {Meta, StoryObj} from '@storybook/react';
+import {Accordion} from "./Accordion";
+import {action} from '@storybook/addon-actions'
+
+const items = [
+    { title: "First item", value: 1 },
+    { title: "Second item", value: 2 },
+    { title: "Third item", value: 3 },
+];
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Accordion> = {
+    title: 'New Version/Accordion',
+    component: Accordion,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        titleValue: 'Menu',
+        value: false,
+        onChange: action('Change accordion mode'),
+        items: [
+            { title: "First item", value: 1 },
+            { title: "Second item", value: 2 },
+            { title: "Third item", value: 3 },
+        ],
+        onClick: action('Change accordion on click it'),
+    },
+}
+
+
+export default meta;
+type Story = StoryObj<typeof Accordion>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const MainAccordion: Story = {};
+export const SecondAccordion: Story = {};*/
+
+/*export const Secondary: Story = {
+    args: {
+        titleValue: 'User',
+        value: false,
+    },
+};*/
+
+
