929d3d402c416a8505fecee1eec205c33d37948f
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-09 21:39:07 +0300

    generate select with div teg

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
+import {inspect} from "util";
+import styles from './select.module.css'
 
-type ItemType={
-    title: string,
+export type ItemType={
+    title: any,
     value: any
 }
 
-type SelectPropsType = {
-    value: any
+export type SelectPropsType = {
+    value: string | undefined
     onChange: (value: any)=> void
     items: ItemType[]
 }
 export function Select(props: SelectPropsType) {
+
+    const [active, setActive] = useState(false)
+    const toggleItem = ()=> setActive(!active)
+    const selectedItem = props.items.find(i=>i.value===props.value)
     //debugger
     return(
         <div>
-            <input type={<select name="" id=""></select>}/>
-           {/*<div>{props.items.filter(el=> el.value===props.value)}</div>*/}
-            {props.items.map(i=> <div>{i.title}</div>)}
+
+            <span className={} onClick={toggleItem}>{selectedItem && selectedItem.title}</span>
+           {/* <input type={<select name="" id=""></select>}/>
+           <div>{props.items.filter(el=> el.value===props.value)}</div>
+            {props.items.map(i=> <div>{i.title}</div>)}*/}
+
+           {/* <select className={styles.select} value={props.value} onChange={props.onChange}>
+            {props.items.map((option) => (
+                <option className={styles.option+ ''+ styles.active} key={option.value} value={option.value}>
+                    {option.title}
+                </option>
+            ))}
+            </select>*/}
+
+
         </div>
     )
-
 }
