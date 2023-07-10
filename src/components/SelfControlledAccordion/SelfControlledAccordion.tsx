321d6cff5db1ff45e3d38f93884d3259be36988e
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-10 23:37:50 +0300

    create reducer for accordion

--- src/components/SelfControlledAccordion/SelfControlledAccordion.tsx
+++ src/components/SelfControlledAccordion/SelfControlledAccordion.tsx
@@ -1,19 +1,36 @@
-import React, {useState} from "react";
+import React, {useReducer, useState} from "react";
+import {reducer, TOGGLE_CONSTANT} from "./reducer";
 
 type UncontrolledAccordionPropsType = {
     titleValue: string
     //collapsed: boolean
 }
+/*type ActionType={
+    type: string,
+
+}
+const TOGGLE_CONSTANT='TOGGLE-COLLAPSED'
+const reducer= (state: boolean, action: ActionType)=>{
+    //debugger
+    switch (action.type){
+        case TOGGLE_CONSTANT:
+            return !state;
+        default:
+            return state
+    }
+}*/
 
 
 export function UncontrolledAccordion (props: UncontrolledAccordionPropsType) {
 
-    let [collapsed, setCollapsed] = useState(false)
+    //let [collapsed, setCollapsed] = useState(false)
+    let [collapsed, dispatch] = useReducer(reducer, false)
 
 
 
         return <div>
-            <AccordionTitle title={props.titleValue} onClick={()=>{setCollapsed(!collapsed)}}/>
+            {/*<AccordionTitle title={props.titleValue} onClick={()=>{setCollapsed(!collapsed)}}/>*/}
+            <AccordionTitle title={props.titleValue} onClick={()=>{dispatch({type: TOGGLE_CONSTANT})}}/>
 
             {!collapsed && <AccordionBody/>}
 
