e2533f4cfd96c342ca677d3abab25d0d2aa90e31
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-11 17:00:10 +0300

    use React.memo

--- src/stories/assets/ReactMemo.stories.tsx
+++ src/stories/assets/ReactMemo.stories.tsx
@@ -1,27 +1,38 @@
-import {useState} from "react";
+import {memo, useState} from "react";
 
 export default {
     title: 'React.memo demo'
 }
 
 
-const NewMessagesCounter=(props: any)=>{
+const NewMessagesCounter = (props: any) => {
     return <div>{props.count}</div>
 }
 
-type UsersType={
+type UsersType = {
     users: Array<string>
 }
-const Users=(props: UsersType)=>{
-    return <div>{props.users.map((u,i)=><div key={i}>{u}</div>)}</div>
+const UsersSecret = (props: UsersType) => {
+    console.log('Users')
+    return <div>{props.users.map((u, i) => <div key={i}>{u}</div>)}</div>
 }
-export const Example1 = ()=>{
- const [counter,setCounter]=useState(0);
- const [users,setUsers]=useState(['Dimych', 'Valera', 'Artem'])
 
-    return<>
-        <button onClick={()=>{setCounter(counter+1)}}>+</button>
-    <NewMessagesCounter count={counter} />
-    <Users users={users} />
- </>
+export const Users = memo(UsersSecret)
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem'])
+
+    const addUser = () => {
+        const newUsers=[...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUsers);
+    }
+    return <>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+        <button onClick={addUser}>add user</button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
+    </>
 }
