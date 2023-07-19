3f46236809c52fbaaa4c9d34c4b1356cc7614d92
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-19 17:19:36 +0300

    create clock component

---
+++ src/components/Clock/CLock.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+
+type PropsType={
+
+}
+
+export const Clock: React.FC<PropsType> = (props) => {
+
+    const [date, setDate] = useState(new Date())
+
+    useEffect(()=>{
+        debugger
+       const intervalID= setInterval(()=>{
+           console.log('TICK')
+            setDate(new Date());
+        },1000)
+        return()=>{
+            debugger
+            clearInterval(intervalID)
+        }
+    },[])
+
+    const get2digitsString = (num: number)=>num<10 ? '0'+num : num
+
+    return <div>
+        <span>{get2digitsString(date.getHours())}</span>
+        :<span>{get2digitsString(date.getMinutes())}</span>
+        :<span>{get2digitsString(date.getSeconds())}</span>
+    </div>
+}
