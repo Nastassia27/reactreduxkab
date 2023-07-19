5d651adf781a94f0ea88880712c92e51492d1bb0
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-19 17:19:58 +0300

    testing clock component in stories

---
+++ src/components/Clock/Clock.stories.tsx
@@ -0,0 +1,17 @@
+import {useEffect, useState} from "react";
+import {Clock} from "./CLock";
+export default {
+    title: 'Clock demo',
+    component: Clock
+}
+
+export const ClocksWithDimychExample = () => {
+    //через секунду что-то обновить
+    console.log('ClocksExample')
+    //const initValue = useMemo(generateDate,[])
+
+
+    return <>
+       <Clock/>
+    </>
+}
