5bbc96fb0db89542fae613a445f9db5b78fb797a
Nastassia27 <anastasia.derugo@gmail.com>
2023-07-19 14:44:19 +0300

    use functions in useState

---
+++ src/stories/assets/UseState.stories.tsx
@@ -0,0 +1,29 @@
+import {memo, useMemo, useState} from "react";
+
+export default {
+    title: 'useState demo'
+}
+
+function generateDate(){
+    console.log('generateDate')
+    return 1
+}
+
+export const Example1 = () => {
+    console.log('Example1')
+        //const initValue = useMemo(generateDate,[])
+    const [counter, setCounter] = useState(generateDate);
+
+    const changer = (state: number)=> state +1
+
+
+    return <>
+        <button onClick={() => {
+            setCounter(changer)
+        }}>+
+        </button>
+
+        {counter}
+
+    </>
+}
